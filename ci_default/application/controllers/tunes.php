2400a3fcb86c3a642f37781332e5dce939fae944
Mason Crane <masonecrane@gmail.com>
2015-04-22 13:36:50 -0600

    added loading gif, as well as used curl library with ajax to complete the assignment

--- ci_default/application/controllers/tunes.php
+++ ci_default/application/controllers/tunes.php
@@ -1,11 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Tunes extends CI_Controller {
-  public function index_html() {
-  	$artist = str_replace(" ", "", $this->input->post());
-  	var_dump($artist);
-  	die();
-  	$this->load->view('partials/index');
+  public function get_movie() {
+  	$artist = str_replace(' ', '', $this->input->post('user_input'));
+  	$url = 'https://itunes.apple.com/search?term='.$artist.'&entity=musicVideo';
+  	$ch = curl_init();
+  	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+  	curl_setopt($ch, CURLOPT_URL, $url);
+  	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  	$data = curl_exec($ch);
+  	$info = curl_getinfo($ch);
+  	curl_close($ch);
+  	echo $data;
   }
 
   public function index() {
--- ci_default/application/views/index.php
+++ ci_default/application/views/index.php
@@ -8,17 +8,29 @@
 		$(document).ready(function() {
 			$('form').submit(function() {
 				$('#video').html('<img src="/assets/images/loading.gif">');
-				$.get('/tunes/index_html', function(res) {
-					$('#video').html(res);
-				});
-				// return false;
+				$.post($(this).attr('action'), $(this).serialize(), function(res) {
+					var html_string = '';
+					if(res.results.length !== 0) {
+						html_string = "<video controls src='"+res.results[0].previewUrl+"'><\/video>";
+					}
+					else {
+						html_string = 'Not Found';
+					}
+					$('#video').html(html_string);
+				}, 'json');
+				return false;
 			});
 		});
 	</script>
+	<style>
+		form {
+			margin-bottom: 1em;
+		}
+	</style>
 </head>
 <body>
 	<h1>Enter Artist's Name:</h1>
-	<form action="/tunes/index_html" method="post">
+	<form action="/tunes/get_movie" method="post">
 		<input type="search" id="user_input" name="user_input">
 		<input type="submit" value="search">
 	</form>
